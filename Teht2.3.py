de7d27106540f23e1284b6ecabac4417e11a44f5
valtsulatvio <valtteri.latvio@metropolia.fi>
2022-08-22 14:29:57 +0300

    2.2 tehty

---
+++ Teht2.3.py
@@ -0,0 +1,7 @@
+import math
+kanta=int (input("Anna suorakulmion kanta:"))
+korkeus=int (input("Anna suorakulmion korkeus:"))
+ala=(kanta*korkeus)
+piiri=(kanta+korkeus)
+print(f"Suorakulmion pinta-ala on: {ala:6.2f}")
+print(f"Suorakulmion piiri on: {piiri:6.2f}")
