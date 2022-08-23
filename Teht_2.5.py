c835453c51ddb411d15e0cbea308ab9bd6f00b36
valtsulatvio <valtteri.latvio@metropolia.fi>
2022-08-23 15:49:32 +0300

    2.5 tehty

--- Teht_2.5.py
+++ Teht_2.5.py
@@ -1,7 +1,16 @@
+import math
 le= float(input("Anna leiviskät: "))
-#na= le/20
-#lu=na/32
-#g=lu*13.3
-#kg=g/1000
-kg=le*8.5
-print(f"Massa nykyarvoilla: {kg:6.1f}")
+nau= float(input("Anna naulat: "))
+luo= float(input("Anna luodit: "))
+p=nau*32
+o=p+luo
+t=o*13.3
+na=le*20
+lu=na*32
+g=lu*13.3
+h=t+g
+kg=h/1000
+#kg=le*8.5
+loput= h-math.floor(kg)*1000
+#math.floor(kg)
+print(f"Massa nykyarvoilla: {kg:2.1f},{loput:2.2f}")
