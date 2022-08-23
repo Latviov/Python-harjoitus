bbd9197ddbabb237e5fd940f2e060e543fa57b8f
valtsulatvio <valtteri.latvio@metropolia.fi>
2022-08-23 14:44:50 +0300

    2.5 kesken 2.6 tehty

---
+++ Teht_2.5.py
@@ -0,0 +1,7 @@
+le= float(input("Anna leiviskät: "))
+#na= le/20
+#lu=na/32
+#g=lu*13.3
+#kg=g/1000
+kg=le*8.5
+print(f"Massa nykyarvoilla: {kg:6.1f}")
---
+++ Teht_2.6.py
@@ -0,0 +1,5 @@
+import random
+randomlist = random.sample(range(1, 9), 3)
+print(randomlist)
+randomlst = random.sample(range(1, 6), 4)
+print(randomlst)
