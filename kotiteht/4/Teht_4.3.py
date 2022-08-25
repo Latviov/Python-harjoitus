d1b4c2d194c0a514a7c62e4435e60932087553bb
valtt <valtteri.latvio@metropolia.fi>
2022-08-25 18:55:59 +0300

    4.3 tehty

---
+++ kotiteht/4/Teht_4.3.py
@@ -0,0 +1,8 @@
+num=int
+while num!="" :
+    lst = list(map(int, input("Anna luku: ").split()))
+    if  == "":
+        break
+print(f"Pienin luku on: {min(lst)} \nSuurin luku on: {max(lst)} ")
+    #if num!="" :
+        #list.append(num)
