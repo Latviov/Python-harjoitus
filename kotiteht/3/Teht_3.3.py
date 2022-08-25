db64ed2a094dca6feca87570756160756a7d30cf
valtt <valtteri.latvio@metropolia.fi>
2022-08-25 14:26:22 +0300

    3.3 tehty

---
+++ kotiteht/3/Teht_3.3.py
@@ -0,0 +1,21 @@
+print("Kirjota pienillä olin laiska tekee koodii")
+suku= input("Oletko mies vai nainen: ")
+
+
+if suku != "mies" or suku != "nainen" :
+    raise SystemExit(print("Koita nyt edes"))
+hemo= float(input("Anna hemoglobiiniarvosi: "))
+if suku == "mies":
+    if hemo >= 134 and hemo <=195:
+        print ("Hemoglobiini normaali")
+    elif hemo <134:
+            print( "Hemoglobiinisi on alhainen")
+    elif hemo < 195:
+                print ("Hemoglobiinisi on korkealla")
+if suku == "nainen":
+    if hemo >= 117 and hemo <=175:
+        print( "Hemoglobiini normaali")
+    elif hemo <117:
+            print( "Hemoglobiinisi on alhainen")
+    elif hemo < 175:
+                print( "Hemoglobiinisi on korkealla")
