cc8ddcc65c3a44f89ea7dc6736126df1068a5707
valtt <valtteri.latvio@metropolia.fi>
2022-08-25 16:49:16 +0300

    3.4 tehty

---
+++ kotiteht/3/Teht_3.4.py
@@ -0,0 +1,10 @@
+vuosi= float(input("Anna vuosiluku: "))
+if vuosi%4==0 :
+    if vuosi%100==0 and vuosi%400==0:
+        print("Karkausvuosi!")
+    elif vuosi%4==0 and vuosi%100!=0 :
+        print("Karkausvuosi")
+    elif vuosi%100==0 and vuosi%400!=0 :
+        print("Ei karkausvuosi!")
+else :
+    print("Ei karkausvuosi!")
