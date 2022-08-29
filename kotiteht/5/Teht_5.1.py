89913cbf10199a53fa84525553241d19c432cfae
valtt <valtteri.latvio@metropolia.fi>
2022-08-29 13:59:11 +0300

    5.1 korjattu

--- kotiteht/5/Teht_5.1.py
+++ kotiteht/5/Teht_5.1.py
@@ -1,8 +1,9 @@
 import random
 x=int(input("Syötä noppien määrä: "))
+summa=0
 
 for noppa in range (x):
     noppa = random.randint(1, 6)
-    if x==0 :
-        break
-    print(f"Lukema on: {noppa}")
+    summa += noppa
+
+print(f"Lukema on: {summa}")
