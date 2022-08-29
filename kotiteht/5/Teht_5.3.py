cb208fec90e5e61fe32203e9ebaff92ed3cfc39e
valtt <valtteri.latvio@metropolia.fi>
2022-08-29 14:19:00 +0300

    5.3 tehty

---
+++ kotiteht/5/Teht_5.3.py
@@ -0,0 +1,14 @@
+import random
+x = int(input("Syötä luku: "))
+flag = False
+
+for i in range(2, x) :
+    t=x%i
+    if t==0 :
+        flag = True
+        break
+
+if flag:
+    print("Luku ei ole alkuluku")
+else :
+    print("Luku on alkuluku")
