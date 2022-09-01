b486554ee72e39d1a59cb39579e897722a499dae
valtt <valtteri.latvio@metropolia.fi>
2022-09-01 09:44:06 +0300

    6.4 tehty

---
+++ kotiteht/6/Teht_6.4.py
@@ -0,0 +1,14 @@
+x=[]
+def listaus():
+    total = 0
+    for i in range(0, len(res)):
+        total = total + res[i]
+    return total
+while True:
+    num = str(input("Anna luku: "))
+    if num =="":
+        break
+    x.append(num)
+    res = [eval(i) for i in x]
+    listaus()
+print(f"Summa:{(listaus())}")
