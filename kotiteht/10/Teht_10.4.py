3f354f5167c2a2f6680ddd46380112c13d5da169
valtt <valtteri.latvio@metropolia.fi>
2022-10-31 21:57:24 +0200

    10.4 tehty

--- kotiteht/10/Teht_10.4.py
+++ kotiteht/10/Teht_10.4.py
@@ -1,71 +1,76 @@
 import random
+
+
 class Auto:
-    def __init__(self,rekkari,huippu):
-        self.rekkari=rekkari
-        self.huippu=huippu
+    def __init__(self, rekkari, huippu):
+        self.rekkari = rekkari
+        self.huippu = huippu
         self.atm = 0
         self.matka = 0
         self.aika = 0
 
-    def kiihdytys(self,nopeus):
-        self.atm=self.atm+nopeus
-        if self.atm>self.huippu:
-            self.atm=self.huippu
-        elif self.atm<0:
-            self.atm=0
+    def kiihdytys(self, nopeus):
+        self.atm = self.atm + nopeus
+        if self.atm > self.huippu:
+            self.atm = self.huippu
+        elif self.atm < 0:
+            self.atm = 0
 
-    def kuljettu_matka(self,tunnit):
-        self.matka+=(self.atm*tunnit)
-
+    def kuljettu_matka(self, tunnit):
+        self.matka += (self.atm * tunnit)
 
     def tulostaAuto(self):
         print(self.rekkari, self.atm, self.matka)
 
-    def autojen_luonti(self):
-        autot=[]
-        for i in range(1, 10):
-            autot.append(Auto("ABC-" + str(i),random.randint(100,200)))
-        return autot
 
 class Kilpailu:
 
-    def __init__(self,nimi,pituus,autot):
-        self.nimi=nimi
-        self.pituus=pituus
-        self.autot=autot
+    def __init__(self, nimi, pituus, autot):
+        self.nimi = nimi
+        self.pituus = pituus
+        self.autot = autot
 
     def tunti_kuluu(self):
-        nopeus=random.randint(-10,15)
-        Auto.kiihdytys(autot,nopeus)
+        for auto in self.autot:
+            auto.kiihdytys(random.randint(-10, 15))
+            auto.kuljettu_matka(1)
 
     def tulosta_tilanne(self):
         for auto in autot:
-            print(Auto.tulostaAuto(auto))
+            auto.tulostaAuto()
 
     def kilpailu_ohi(self):
         for auto in self.autot:
-            if Auto.kuljettu_matka(auto,tunti) >= self.pituus:
+            if auto.matka >= self.pituus:
                 return True
             else:
                 continue
 
-autot = Auto.autojen_luonti()
-tunti = 0
-romuralli=Kilpailu("Suuri romuralli", 8000,autot)
-while True:
-    Kilpailu.tunti_kuluu()
 
-    if Kilpailu.kilpailu_ohi():
+def autojen_luonti():
+    autot = []
+    for i in range(10):
+        autot.append(Auto("ABC-" + str(i + 1), random.randint(100, 200)))
+    return autot
+
+
+autot = autojen_luonti()
+tunti = 0
+romuralli = Kilpailu("Suuri romuralli", 8000, autot)
+while True:
+    romuralli.tunti_kuluu()
+
+    if romuralli.kilpailu_ohi():
         print("Kilpailu ohi!\n")
-        Kilpailu.tulosta_tilanne()
+        romuralli.tulosta_tilanne()
         break
 
     else:
-        tunti+=1
+        tunti += 1
 
         if tunti % 10 == 0:
             print(f"Tunti: {tunti}\n")
-            Kilpailu.tulosta_tilanne()
+            romuralli.tulosta_tilanne()
 
         else:
             continue
