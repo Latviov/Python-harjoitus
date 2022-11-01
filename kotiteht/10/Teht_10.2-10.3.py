c34222ed966050cde6f269ef86565683bda35e93
valtt <valtteri.latvio@metropolia.fi>
2022-11-01 10:27:46 +0200

    10.2-10.3 korjattu

--- kotiteht/10/Teht_10.2-10.3.py
+++ kotiteht/10/Teht_10.2-10.3.py
@@ -10,21 +10,23 @@ class Hissi:
         elif self.atm > kerros:
             Hissi.kerros_alas(self, kerros)
 
-    def kerros_ylos(self, floor):
-        while self.atm != floor and floor<self.ylin:
+    def kerros_ylos(self, kerros):
+        while self.atm != kerros and kerros<self.ylin:
             Hissi.tulostus(self)
             self.atm += 1
-            if floor<self.ylin:
+            if kerros>self.ylin:
                 self.atm=self.ylin
-            if self.atm == floor:
+                break
+            if self.atm == kerros:
                 Hissi.tulostus(self)
                 print("tääl ollaa")
+                break
 
-    def kerros_alas(self, floor):
-        while self.atm != floor:
+    def kerros_alas(self, kerros):
+        while self.atm != kerros:
             Hissi.tulostus(self)
             self.atm -= 1
-            if self.atm == floor:
+            if self.atm == kerros:
                 Hissi.tulostus(self)
                 print("tääl ollaa")
 
@@ -40,26 +42,32 @@ class Talo:
         self.luku = luku
         self.atm = atm
         self.hissit = []
-        for i in range(1, 4):
+        for i in range(luku):
            self.hissit.append(Hissi(1, 50, 1))
 
     def palo(self):
+        print("Palohälytys!!")
         for i in self.hissit:
             i.siirry_kerrokseen(self.alin)
-            print("Palohälytys!!")
+            Hissi.tulostus(i)
+
     def aja(self, numero, kohde):
         elevator = self.hissit[numero - 1]
         print(f"Ajetaan hissiä {numero}\n")
-        if kohde == 112:
-            self.palo()
         elevator.siirry_kerrokseen(kohde)
+
 
         # Hissi.siirry_kerrokseen(numero, kohde)
 
 
 kerros = 1
-kys = Talo(1, 50, 3, 1)
+kys = Talo(1, 50, 6, 1)
 while kerros < 51:
     hissi_nr=int(input("Valitse hissi"))
     kohde = int(input("Mihin kerrokseen haluat?"))
+    if kohde>50 and kohde!=112:
+        break
+    if kohde == 112:
+        kys.palo()
+        break
     kys.aja(hissi_nr, kohde)
